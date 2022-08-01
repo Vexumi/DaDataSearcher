e15a5681acc0bbeb104ec90e3d82efa9c64f7691
Vexumi <glebas.35211@gmail.com>
2022-08-01 14:01:21 +0700

    UI struct. Created Menu and Settings

--- main.py
+++ main.py
@@ -1,11 +1,58 @@
 import sqlite3
+import requests
+
 
 def get_address():
-    conn = sqlite3.connect("settings.db")
-    cur = conn.cursor()
-    settings = cur.execute("SELECT * FROM settings").fetchone()
-    print(settings)
-    conn.close()
+    while True:
+        settings = get_settings_from_sql()
+        print("//Меню//")
+        print("1) Выход\n"
+              "2) Настройки\n"
+              "Для получения координат введите адрес в свободной форме")
+
+        response = input()
+        if (response == "2"):
+            change_settings(settings)
+            settings = get_settings_from_sql()
+            continue
+        elif (response == "1"):
+            return
+
+
+def get_settings_from_sql(sql_file="settings.db"):
+    cur = sqlite3.connect(sql_file).cursor()
+    response = cur.execute("""SELECT * FROM settings""").fetchone()
+    cur.connection.close()
+    return response
+
+
+def save_settings_to_sql(settings, sql_file="settings.db"):
+    cur = sqlite3.connect(sql_file).cursor()
+    cur.execute("""UPDATE settings
+                    SET URL=?,
+                        API_KEY=?,
+                        Language=?""", settings)
+    cur.connection.commit()
+    cur.connection.close()
+
+
+def change_settings(settings):
+    print("//Настройки//")
+    print(f"1) URL адрес: {settings[0]}\n"
+          f"2) API ключ: {settings[1]}\n"
+          f"3) Язык: {settings[2]}")
+    print('Для изменения введите данные в формате "<номер_поля> <новое_значение>"\n'
+          'Для выхода из настроек нажмите ENTER')
+
+    response = input().split()
+    if len(response) == 2:
+        settings = list(settings)
+        settings[int(response[0]) - 1] = response[1]
+        try:
+            save_settings_to_sql(settings)
+            print("Данные успешно изменены!")
+        except sqlite3.IntegrityError as e:
+            print('Ошибка! Поле "Язык" может содержать в себе значение "ru" или "en". Попробуйте снова')
 
 
 if __name__ == '__main__':
