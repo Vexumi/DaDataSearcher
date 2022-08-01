28c55b3c41d756dd2c16c0c52e84ea70d01d058b
Vexumi <glebas.35211@gmail.com>
2022-08-01 12:39:09 +0700

    Database added

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sqlite3
+
+def get_address():
+    conn = sqlite3.connect("settings.db")
+    cur = conn.cursor()
+    settings = cur.execute("SELECT * FROM settings").fetchone()
+    print(settings)
+    conn.close()
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    get_address()
